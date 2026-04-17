9ce61a836f3aeac6691ff3ceb3bf9f2670af9413
Santhosh1292 <santhoshkamireddy59@gmail.com>
2026-04-17 16:27:29 +0530

    bakend url given

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API = axios.create({
-    baseURL:"http://localhost:5000/api"
+    baseURL:"https://mern-ecommerce-1-gs5q.onrender.com/api"
 })
 API.interceptors.request.use((req)=>{
     const token=localStorage.getItem("token")
